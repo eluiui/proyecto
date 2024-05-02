0c06fa7b10a1b9967dcddabdd865331f560a10fe
eluiui <119857344+eluiui@users.noreply.github.com>
2024-05-02 12:04:56 +0200

    feat: change of clases
    change in clases

---
+++ src/main/java/edu/badpals/proyecto/GildedRose.java
@@ -0,0 +1,20 @@
+package edu.badpals.proyecto;
+
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class GildedRose 
+{
+    List<Updateable> items= List.of();
+    public static void main( String[] args )
+    {
+        
+    }
+
+    public GildedRose (){
+
+    }
+}
---
+++ src/main/java/edu/badpals/proyecto/Updateable.java
@@ -0,0 +1,7 @@
+package edu.badpals.proyecto;
+public interface Updateable {
+    public void update_item();
+    public void update_quality();
+    public void update_sell_in();
+
+}
